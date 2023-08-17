5e03e125a2c6d9631fdc2d2b2b667c6a340203fc
Yuriibe <direxpvp@gmail.com>
2023-08-17 20:56:38 +0200

    - added CLi to select added account

--- loginController.py
+++ loginController.py
@@ -1,14 +1,27 @@
 import pyperclip
 import keyboard
 import time
-
+import os
 from loginData import userData
 
-def enterLoginCredentials():
-    pyperclip.copy(userData[0]["username"])
+def enterLoginCredentials(loginData):
+    pyperclip.copy(loginData['username'])
     keyboard.press_and_release('ctrl+v')
     keyboard.press_and_release('tab')
     time.sleep(2)
-    pyperclip.copy(userData[0]["password"])
+    pyperclip.copy(loginData['password'])
     keyboard.press_and_release('ctrl+v')
     keyboard.press_and_release('enter')
+
+def getUserData():
+    if os.path.isfile("loginData.py"):
+        for index, data in enumerate(userData):
+            username = data["username"]
+            print(f"{index} : Username: {username}")
+        accountId = input("which Account you wanna use your Chair on: ")
+        return userData[int(accountId)]
+    else:
+        print("doesnt exist")
+        with open("loginData.py", "w") as file:
+            pass
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygetwindow as gw
 from programmController import *
 from loginController import *
+import tkinter
 
 leagueClient = "LeagueClient.exe"
 riotClient = "RiotClientServices.exe"
@@ -8,22 +9,26 @@ riotClient = "RiotClientServices.exe"
 
 def main():
     if is_program_running(riotClient) and not is_program_running(leagueClient):
+        loginData = getUserData()
         print(f"{riotClient} is running.")
         try:
             winRiot = gw.getWindowsWithTitle('Riot Client')[0]
             winRiot.activate()
-            enterLoginCredentials()
+            enterLoginCredentials(loginData)
         except:
             kill_process(riotClient)
             main()
     else:
+        loginData = getUserData()
         if is_program_running(leagueClient):
             winLeague = gw.getWindowsWithTitle('League Of Legends')[0]
             winLeague.close()
             kill_process(riotClient)
         startRiotClient()
         time.sleep(5)
-        enterLoginCredentials()
+        enterLoginCredentials(loginData)
         print(f"{riotClient} is not running.")
 
-main()
+
+if __name__ == "__main__":
+    main()
