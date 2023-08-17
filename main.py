3b28fc31798ffa6e0e82b2d6c67b13784c3f262d
Yuriibe <direxpvp@gmail.com>
2023-08-17 19:24:15 +0200

    - added main function - added a check if the league client is running to close it before opening a new riot client instance - added function to kill the riot client if it stays open in the background after closing league client

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 import time
 import pyperclip
 import keyboard
+import pygetwindow as gw
 import os
 
 # Path to the League of Legends shortcut
@@ -9,8 +10,9 @@ shortcut_path = r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Riot Game
 
 # Path to RiotClientServices.exe
 riot_client_path = r'C:\Riot Games\Riot Client\RiotClientServices.exe'
+leagueClient = "LeagueClient.exe"
+riotClient = "RiotClientServices.exe"
 
-program_name = "RiotClientServices.exe"
 
 def startRiotClient():
     subprocess.Popen([shortcut_path], shell=True)
@@ -29,15 +31,35 @@ def is_program_running(program_name):
     try:
         # Run a command to get the list of all running processes
         output = subprocess.check_output("tasklist", shell=True).decode()
-        print(output)
         # Check if the program name is in the list of running processes
         return program_name in output
     except Exception as e:
         print(f"An error occurred: {e}")
         return False
 
+def kill_process(process_name):
+    os.system(f"taskkill /f /im {process_name}")
 
-if is_program_running(program_name):
-    print(f"{program_name} is running.")
-else:
-    print(f"{program_name} is not running.")
+
+
+
+def main():
+    if is_program_running(riotClient) and not is_program_running(leagueClient):
+        print(f"{riotClient} is running.")
+        try:
+            winRiot = gw.getWindowsWithTitle('Riot Client')[0]
+            winRiot.activate()
+            enterLoginCredentials()
+        except:
+            kill_process(riotClient)
+            main()
+    else:
+        if is_program_running(leagueClient):
+            win = gw.getWindowsWithTitle('League Of Legends')[0]
+            win.close()
+            kill_process(riotClient)
+        startRiotClient()
+        enterLoginCredentials()
+        print(f"{riotClient} is not running.")
+
+main()
